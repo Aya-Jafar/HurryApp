c3802bf9e8ac05e705cffab66fe6c339c58fb1ce
Aya-jafar <aya.jafar002@gmail.com>
2024-01-20 19:15:33 +0300

    removing props from  component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "react-slideshow-image/dist/styles.css";
 import Cards from "../components/Cards";
 import Cover from "../components/Cover";
-import { getVideosInfo } from "../services/api";
 
 function Home() {
-  const videos = getVideosInfo();
-
-
   return (
     <>
-      <Cover videos={videos} />
+      <Cover />
 
       <h2 className="cards-catogary-title">
-        <strong style={{color:"#01E8FE"}}>Live</strong> streams we think you'll like
+        <strong style={{ color: "#01E8FE" }}>Live</strong> streams we think
+        you'll like
       </h2>
 
-      <Cards videos={videos} />
+      <Cards />
 
       <h2 className="cards-catogary-title">
         Recorded streams we think you'll like
       </h2>
 
-      <Cards videos={videos} />
+      <Cards />
     </>
   );
 }
