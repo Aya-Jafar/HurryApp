8a5beaa48cc4f0cd628785c5bcb939b31bd5bb65
Aya-jafar <aya.jafar002@gmail.com>
2024-01-20 19:13:12 +0300

    removing mock videos data

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import useAuth from "../store/useAuth";
 export const login = (formData) => {
   const { setIsLoggedIn, setIsLoginModalOpen } = useAuth.getState();
 
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/api/auth/signin`, {
+  fetch(`http://127.0.0.1:8000/api/auth/signin`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -22,7 +22,6 @@ export const login = (formData) => {
         setIsLoggedIn(true);
         setIsLoginModalOpen(false);
       }
-      console.log("successful", data);
     })
     .catch((error) => {
       console.error("Error signing up:", error.message);
@@ -32,7 +31,7 @@ export const login = (formData) => {
 export const signup = (formData) => {
   const { setIsLoggedIn, setIsSignUpModalOpen } = useAuth.getState();
 
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/api/auth/signup`, {
+  fetch(`http://127.0.0.1:8000/api/auth/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -47,43 +46,62 @@ export const signup = (formData) => {
     })
     .then((data) => {
       if (data.token.access) {
-        localStorage.setItem("token", data.token.access);
+        localStorage.setItem("token", data?.token?.access);
+        localStorage.setItem("userName", data?.name);
         setIsLoggedIn(true);
         setIsSignUpModalOpen(false);
       }
-      console.log("Signup successful", data);
     })
     .catch((error) => {
       console.error("Error signing up:", error.message);
     });
 };
 
-export const getVideosInfo = () => {
-  const videos = [
-    {
-      id: 1,
-      title: "Title 1",
-      url: "https://www.youtube.com/embed/CAt2-HEIpCI?si=hQ_sdxu0vgFXlWLU",
-      userName: "user 1",
-    },
-    {
-      id: 2,
-      title: "Title 2",
-      url: "https://www.youtube.com/embed/wZreAuJwMZ8?si=Ve3EYEk3i6N8hSbV",
-      userName: "user 2",
-    },
-    {
-      id: 3,
-      title: "Title 3",
-      url: "https://www.youtube.com/embed/JY2McN-MZFc?si=vuGDN_mMxd5yEImT",
-      userName: "user 3",
-    },
-    {
-      id: 4,
-      title: "Title 4",
-      url: "https://www.youtube.com/embed/nD6qsOPTaKQ?si=95TMVUOxzMlf4FmX",
-      userName: "user 4",
-    },
-  ];
-  return videos;
+export const getVideosInfo = async (setVideosList) => {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/api/content/get_all_videos`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.length > 0) {
+      setVideosList(data);
+    }
+  } catch (error) {
+    console.error("Error fetching videos:", error.message);
+  }
+};
+
+export const getVideoById = async (id, setVideo) => {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:8000/api/content/get_video/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    setVideo(data);
+  } catch (error) {
+    console.error("Error fetching videos:", error.message);
+  }
 };
