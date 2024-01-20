10d6ed4693e018117a3e6e238a9273537b694662
Aya-jafar <aya.jafar002@gmail.com>
2024-01-20 19:17:10 +0300

    setting the fetched videos from the api

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,31 +1,19 @@
-import React from "react";
-import avatarImage from "../images/Profile.webp";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getVideosInfo } from "../services/api";
+import VideoCard from "./VideoCard";
 
-function Cards({ videos }) {
+function Cards() {
+  const [videos, setVideosList] = useState([]);
+
+  useEffect(() => {
+    getVideosInfo(setVideosList);
+  }, []);
+
   return (
     <>
       <div className="cards">
-        {videos.map((video, index) => (
-          <div key={index} className="card">
-            <Link to={`/stream/${video.id}`}>
-              <iframe
-                width="100%"
-                height="100%"
-                src={video.url}
-                frameBorder="0"
-                allowFullScreen
-              ></iframe>
-
-              <div className="user-info">
-                <img src={avatarImage} />
-                <div>
-                  <h3>{video.title}</h3>
-                  <p>{video.userName}</p>
-                </div>
-              </div>
-            </Link>
-          </div>
+        {videos?.map((video, index) => (
+          <VideoCard video={video}/>
         ))}
       </div>
     </>
